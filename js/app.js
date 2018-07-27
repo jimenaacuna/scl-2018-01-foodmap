aa9f291e992ca8079493535487e8cafc77a75ae6
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-07-27 00:41:23 -0400

    algunos avances

--- js/app.js
+++ js/app.js
@@ -1,13 +1,40 @@
 window.onload = () => {
-    initMap();
+
+var position = window.navigator.geolocation;
+var opciones = {
+    enableHighAccuracy:true,
+    timeout:45000}
+
+navigator.geolocation.getCurrentPosition(mostrarCoords,error,opciones)
 }
 
-function initMap(){
-    var options = {
-        zoom:8,
-        center:{lat:42.3601,lng:-71.0589}
-    }
 
-    var map = new google.maps.map(document.getElementById('map'), options);
 
+function mostrarCoords(pos){
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+
+};
+
+map = new google.maps.Map(document.getElementById('map'), {
+        zoom:20, 
+        center:punto,
+        mapTypeId:google.maps.mapTypeId.ROADMAP
+
+
+    });
+
+var punto = new google.maps.latLng(pos.coords.latitude,pos.coords.longitude)
+
+        
+let marca = new google.maps.Marker({
+        icon:"img/ubicacion.png",
+        position:punto,
+        map:mapa,
+        title:"Aqui estás"})    
+    
+function error(err){
+    alert(err)
 }
+
+
